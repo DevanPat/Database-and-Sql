bcda45fda2e4c4bd15bd1228a3f0e912a29d0548
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-03-31 01:36:14 -0500

    Update SQLQuery1.sql

--- SQL Server Management Studio/SQLQuery1.sql
+++ SQL Server Management Studio/SQLQuery1.sql
@@ -1,4 +1,135 @@
 
+--CREATE TABLE tbl_animalia (
+--	animalia_id INT PRIMARY KEY NOT NULL IDENTITY (1,1),
+--	animalia_type VARCHAR(30) NOT NULL
+--);
+
+--INSERT INTO tbl_animalia
+--(animalia_type)
+--VALUES
+--('vertebrate'),
+--('invertebrate')
+--;
+
+--CREATE TABLE tbl_class (
+--class_id INT PRIMARY KEY NOT NULL IDENTITY (100,1),
+--class_type VARCHAR(50) NOT NULL
+--);
+
+--INSERT INTO tbl_class
+--(class_type)
+--VALUES
+--('bird'),
+--('reptilian'),
+--('mammal'),
+--('arthropod'),
+--('fish'),
+--('worm'),
+--('cnidaria'),
+--('echinoderm')
+--;
+
+
+--UPDATE tbl_class SET class_type = 'birds' WHERE class_type = 'bird';
+
+--SELECT REPLACE(class_type, 'bird', 'birds') FROM tbl_class;
+
+--SELECT class_type FROM tbl_class WHERE class_type = 'bird';
+
+--SELECT UPPER(class_type) FROM tbl_class WHERE class_type = 'bird';
+
+--SELECT COUNT(class_type) FROM tbl_class WHERE class_type = 'bird';
+
+--CREATE TABLE tbl_persons (
+--	persons_id INT PRIMARY KEY NOT NULL IDENTITY (1,1),
+--	persons_fname VARCHAR(50) NOT NULL,
+--	persons_lname VARCHAR(50) NOT NULL,
+--	persons_contact VARCHAR(50) NOT NULL
+--);
+
+--INSERT INTO tbl_persons
+--	(persons_fname, persons_lname, persons_contact)
+--	VALUES
+--	('bob', 'smith', '232-345-5768'),
+--	('mary', 'ann', '232-345-5768'),
+--	('tex', 'burns', '232-345-5768'),
+--	('gerry', 'kerns', '232-345-5768'),
+--	('sally', 'fields', '232-345-5768')
+--;
+
+--SELECT persons_fname, persons_lname, persons_contact FROM tbl_persons WHERE persons_id BETWEEN 2 AND 5;
+--SELECT persons_fname, persons_lname, persons_contact FROM tbl_persons WHERE persons_lname LIKE 'ke%';
+
+--SELECT * FROM tbl_class
+
+--SELECT persons_fname, persons_lname, persons_contact FROM tbl_persons WHERE persons_lname LIKE '_u%s';
+
+--UPDATE tbl_persons SET persons_fname = 'mars' WHERE persons_fname = 'bob';
+
+--SELECT persons_fname, persons_lname, persons_contact FROM tbl_persons WHERE persons_fname LIKE 'm%' ORDER BY persons_lname;
+
+--SELECT persons_fname AS 'First Name', persons_lname AS 'Last Name', persons_contact AS 'Phone:'
+--	FROM tbl_persons WHERE persons_fname LIKE 'm%' ORDER BY persons_lname;
+
+--	DELETE FROM tbl_persons WHERE persons_lname = 'Smith';
+
+--	DROP TABLE tbl_persons;
+
+--CREATE TABLE tbl_order (
+--order_id INT PRIMARY KEY NOT NULL IDENTITY (1,1),
+--order_type VARCHAR(50) NOT NULL
+--);
+
+--CREATE TABLE tbl_care	(
+--care_id VARCHAR(50) PRIMARY KEY NOT NULL,
+--care_type VARCHAR(50) NOT NULL,
+--care_specialist INT NOT NULL
+--);
+
+--CREATE TABLE tbl_nutrition	(
+--	nutrition_id INT PRIMARY KEY NOT NULL IDENTITY (2200,1),
+--	nutrition_type VARCHAR(50) NOT NULL,
+--	nutrition_cost MONEY NOT NULL
+--);
+
+--CREATE TABLE tbl_habitat	(
+--habitat_id INT PRIMARY KEY NOT NULL IDENTITY (5000,1),
+--habitat_type VARCHAR(50) NOT NULL,
+--habitat_cost MONEY NOT NULL
+--);
+
+--CREATE TABLE tbl_specialist	(
+--specialist_id INT PRIMARY KEY NOT NULL IDENTITY (1,1),
+--specialist_fname VARCHAR(50) NOT NULL,
+--specialist_lname VARCHAR(50) NOT NULL,
+--specialist_contact VARCHAR(50) NOT NULL
+--);
+
+--INSERT INTO tbl_order
+--	(order_type)
+--	VALUES 
+--	('carnivore'),
+--	('herbivore'),
+--	('omnivore')
+--;
+
+--SELECT * FROM tbl_order
+
+--INSERT INTO tbl_care 
+--	(care_id, care_type, care_specialist)
+--	VALUES
+--	('care_0', 'replace the straw', 1),
+--	('care_1', 'repaire or replace broken toys', 4),
+--	('care_2', 'bottle feed vitamins', 1),
+--	('care_3', 'human contact_pet subject', 2),
+--	('care_4', 'clean up animal waste', 1),
+--	('care_5', 'move subject to exercise pen', 3),
+--	('care_6', 'drain and refill aquarium', 1),
+--	('care_7', 'extensive dental work', 3)
+--;
+
+--SELECT * FROM tbl_care
+
 --INSERT INTO tbl_nutrition
 --	(nutrition_type, nutrition_cost)
 --	VALUES
@@ -12,6 +143,7 @@
 --	('vitamin and marrow', 3500)
 --;
 
+--	SELECT * FROM tbl_nutrition;
 
 --	INSERT INTO tbl_habitat
 --	(habitat_type, habitat_cost)
@@ -26,7 +158,8 @@
 --	('cliff with shaded cave', 15000)
 --;
 
---SELECT * FROM tbl_habitat
+--SELECT * FROM tbl_habitat;
+
 
 --INSERT INTO tbl_specialist
 --(specialist_fname, specialist_lname, specialist_contact)
@@ -64,4 +197,29 @@
 --	('grey wolf', 1, 102, 1, 5001, 2201, 'care_4')
 --;
 
---SELECT * FROM tbl_species;
+SELECT * FROM tbl_species WHERE species_name = 'chicken';
+
+SELECT
+	a1.species_name, a2.animalia_type,
+	a3.class_type, a4.order_type, a5.habitat_type,
+	a6.nutrition_type, a7.care_type
+	FROM tbl_species a1
+	INNER JOIN tbl_animalia a2 ON a2.animalia_id = a1.species_animalia
+	INNER JOIN tbl_class a3 ON a3.class_id = a1.species_class
+	INNER JOIN tbl_order a4 ON a4.order_id = a1.species_order
+	INNER JOIN tbl_habitat a5 ON a5.habitat_id = a1.species_habitat
+	INNER JOIN tbl_nutrition a6 ON a6.nutrition_id = a1.species_nutrition
+	INNER JOIN tbl_care a7 ON a7.care_id = a1.species_care
+	WHERE species_name = 'brown bear'
+;
+
+SELECT 
+	a1.species_name, a2.habitat_type, a2.habitat_cost,
+	a3.nutrition_type, a3.nutrition_cost
+	FROM tbl_species a1
+	INNER JOIN tbl_habitat a2 ON a2.habitat_id = a1.species_habitat
+	INNER JOIN tbl_nutrition a3 ON a3.nutrition_id = a1.species_nutrition
+	WHERE species_name = 'ghost bat'
+;
+SELECT * FROM tbl_species;
+
