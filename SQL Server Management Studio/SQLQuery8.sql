064da9a2b07db119fa5d876df316f066b5c934be
DevanPat <50036015+DevanPat@users.noreply.github.com>
2020-04-04 19:30:02 -0500

    Update SQLQuery8.sql

--- SQL Server Management Studio/SQLQuery8.sql
+++ SQL Server Management Studio/SQLQuery8.sql
@@ -14,12 +14,12 @@ WHERE nutrition_cost < 600.00;
 SELECT * FROM tbl_nutrition WHERE nutrition_id BETWEEN 2202 AND 2206;
 
 SELECT species_name AS "Species Name:", nutrition_type AS "Nutrition Type:"
-FROM tbl_nutrition, tbl_species 
-WHERE species_name = nutrition_type;
+FROM tbl_nutrition 
+FULL OUTER JOIN tbl_species
+ON species_id = nutrition_id
 
+SELECT specialist_contact, specialist_fname, specialist_lname, species_name, species_care
+FROM tbl_specialist
+FULL OUTER JOIN tbl_species
+ON species_care = 'care_4'
 
-SELECT tbl_specialist.specialist_contact, tbl_specialist.specialist_fname, tbl_specialist.specialist_lname, tbl_species.species_name, tbl_species.species_care
-FROM tbl_specialist, tbl_species
-WHERE tbl_species.species_care=tbl_specialist.specialist_fname;
-
-SELECT * FROM tbl_species
